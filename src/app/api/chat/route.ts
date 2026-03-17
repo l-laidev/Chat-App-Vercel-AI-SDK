9fdccb85ab6a3ce1d4ab1290a7674ff42fc4be46
H.A. <114867851+l-laidev@users.noreply.github.com>
2026-03-17 14:15:14 +0500

    add Zod schema validation, advanced usage of streamText

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,16 +1,44 @@
-import { google } from "@ai-sdk/google";
-import { type UIMessage, convertToModelMessages, streamText } from "ai";
+import { models } from "@/lib/ai/models";
+import { SYSTEM_PROMPT } from "@/lib/ai/prompts";
+import { type UIMessagePart, UIDataTypes, type UITools, convertToModelMessages, streamText } from "ai";
+import { z } from 'zod';
+
 
 export const runtime = 'edge';  // lower latency
+export const maxDuration = 30;
+
+const chatRequestSchema = z.object({
+    messages: z.array(z.object({
+        role: z.enum(['user', 'assistant', 'system']),
+        text: z.string(),
+        parts: z.array(z.custom<UIMessagePart<UIDataTypes, UITools>>()),
+    })),
+    model: z.enum(['gemini-2.5-flash-lite', 'gemini-2.5-flash']).default('gemini-2.5-flash'),
+    temperature: z.number().min(0).max(2).default(0.7),
+});
 
 export async function POST(req:Request) {
-    const { messages }: { messages: UIMessage[] } = await req.json();
+    try {
+        const body = await req.json();
+        const { messages, model, temperature } = chatRequestSchema.parse(body);
+        
+        const result = streamText({
+            model: models[model],
+            messages: await convertToModelMessages(messages),
+            system: SYSTEM_PROMPT,
+            temperature,
+            maxOutputTokens: 4096,
+            // signal for client disconnection
+            abortSignal: req.signal,
+            // analytics
+            onFinish: async ({text, usage}) => {
+                console.log(`Complete: ${usage.totalTokens} tokens.`)
+            }
+        })
 
-    const results = streamText({
-        model: google("gemini-2.5-flash-lite"),
-        messages: await convertToModelMessages(messages),
-        system: `You are a helpful AI assistant. Be concise and clear.`,
-    })
+        return result.toUIMessageStreamResponse();
 
-    return results.toUIMessageStreamResponse();
+    } catch (error) {
+        
+    }
 }
